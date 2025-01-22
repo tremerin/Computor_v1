fcd42a7b95a34fceae6ca7e4d2534e711355c49e
tremerin <tremerin13@gmail.com>
2025-01-22 15:11:34 +0100

    parseo monomios

--- regex.py
+++ regex.py
@@ -2,22 +2,23 @@ import re
 import sys
 
 #expresiones regulares
-regex = r" ?[+-]? ?\d+(\.\d+)? X\^\d+"
-len:int = len(sys.argv[1])
-temp:str
-monomios = list()
-print(type(monomios))
-print("len:", len)
-span = re.match(regex, sys.argv[1])
-print(span)
-if span == None:
+if len(sys.argv) != 2:
+    print("Error: número de argumentos")
     exit()
-monomio = sys.argv[1][span.span()[0]:span.span()[1]]
-print("monomio:", monomio)
-monomios.append(monomio)
-#print(re.findall(regex, sys.argv[1]))
-if span.span()[1] < len:
-    temp = sys.argv[1][span.span()[1]:len-1]
-    print(temp)
+regex = r" ?[+-]? ?\d+(\.\d+)? \* X\^\d+"
+leng:int = len(sys.argv[1])
+temp:str = sys.argv[1]
+monomials = list()
+pos_end:int = 0
+
+while len(temp) > 0:
     span = re.match(regex, temp)
-    print(span)
+    if span == None:
+            print("Error: sintaxis")
+            exit()
+    pos_end = span.span()[1]
+    monomials.append(temp[span.span()[0]:pos_end])
+    print(monomials[-1])
+    temp = temp[pos_end +1:leng]
+
+print(monomials)
