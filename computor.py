b852e86c0eb52efe921d8caa3d4606835a5f8961
tremerin <tremerin13@gmail.com>
2025-01-27 13:24:28 +0100

    second_degree_equation

--- computor.py
+++ computor.py
@@ -19,6 +19,17 @@ if len(sys.argv) == 4:
         x2 = (-b - math.sqrt(dis)) / (2 * a)
         print(f"x2: {x2}")
 
+def second_degree_equation(a:float, b:float, c:float):
+    dis = (b * b) - (4 * c * a)
+    if dis <0:
+        print("No tiene solucion real")
+    else:
+        #calcular soluciones
+        x1 = (-b + math.sqrt(dis)) / (2 * a)
+        print(f"x1: {x1}")
+        x2 = (-b - math.sqrt(dis)) / (2 * a)
+        print(f"x2: {x2}")
+
 
 #computor 
 equation_terms = sys.argv[1].split(" =")
@@ -83,4 +94,7 @@ print(f"Polynomial degree: {reduced_form[-1][1]}")
 if reduced_form[-1][1] > 2:
     print("The polynomial degree is strictly greater than 2, I can't solve.")
     exit()
-
+elif reduced_form[-1][1] == 2:
+    second_degree_equation(reduced_form[2][0], reduced_form[1][0], reduced_form[0][0])
+else:
+    print("primer grado")
