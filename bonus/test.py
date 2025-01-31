b290a08ba188f8b22620ab7a0c5aacdd59e9a39c
tremerin <tremerin13@gmail.com>
2025-01-31 20:52:10 +0100

    mcm

--- bonus/test.py
+++ bonus/test.py
@@ -18,3 +18,31 @@ for string in split_string:
         print(f"{string}: false")
     else:
         print(f"{string}: true")
+
+
+#minimo comun multiplo
+def mcm(nums:list):
+    max_multiplo = nums[0]
+    mach:bool = False
+    if len(nums) == 1:
+        return max_multiplo
+    for i in range(len(nums) - 1):
+        print(i)
+        while mach == False:
+            multiplo = max(max_multiplo, nums[i + 1])
+            if multiplo % nums[i] == 0 and multiplo % nums[i + 1] == 0:
+                max_multiplo = multiplo
+                mach = True
+            else:
+                max_multiplo += 1
+        
+    return max_multiplo
+
+
+#maximo comun divisor
+def mcd(nums:list):
+    pass
+
+nums = [2, 6, 5]
+print(f"mcm de: {nums}")
+print(mcm(nums))
